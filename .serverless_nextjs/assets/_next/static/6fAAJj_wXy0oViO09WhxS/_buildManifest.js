3570fb69a533011add6ae9f9ae79dc88b842237a
HugoB2409 <belanger.hugo@hotmail.com>
2021-03-22 15:27:24 -0400

    test serverless et github action

---
+++ .serverless_nextjs/assets/_next/static/6fAAJj_wXy0oViO09WhxS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-a9a285da8268af08ffd3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0333995c6ede2dfc42ad.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
